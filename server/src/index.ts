f2154744fc8b04f83f1b4a949f7f170095e5c38b
seth <seth@seth.seth>
2025-10-23 07:13:37 +0300

    made it work i thank God

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,14 +13,19 @@ import productsRoutes from './routes/products'
 
 dotenv.config({ path: './.env' })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5500
 const MONGODB_URI = process.env.MONGODB_URI || ''
 
 const app = express()
 
 // Middlewares
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
+}))
 app.use(express.json())
 app.use(morgan('dev'))
 
